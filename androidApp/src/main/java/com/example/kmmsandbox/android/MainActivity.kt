88df6490e5b7338633df6cf518daa65c5649cc38
svetlana.isakova <svetlana.isakova@jetbrains.com>
2022-09-12 19:12:12 +0200

    Created project

---
+++ androidApp/src/main/java/com/example/kmmsandbox/android/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.kmmsandbox.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kmmsandbox.Greeting
+import com.example.kmmsandbox.android.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting(Greeting().greeting())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        Greeting("Hello, Android!")
+    }
+}
